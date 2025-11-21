1c7605ce4e486cdd798cf63735ba86dbdb4c1d2d
parisan <parisan0487@gmail.com>
2025-11-21 23:49:32 +0330

    login

--- src/app/(application)/admin/login/page.jsx
+++ src/app/(application)/admin/login/page.jsx
@@ -1,17 +1,18 @@
+export const dynamic = 'force-dynamic';
 import { cookies } from 'next/headers';
 import { verifyToken } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 import LoginForm from './LoginForm';
 
-
-
 export default function AdminLoginPage() {
-    const token = cookies().get('token')?.value;
+    const cookieStore = cookies();
+    const token = cookieStore.get('token')?.value;
+
     const isValid = verifyToken(token);
 
     if (isValid) {
         redirect('/admin');
     }
 
-    return <LoginForm/>;
+    return <LoginForm />;
 }
